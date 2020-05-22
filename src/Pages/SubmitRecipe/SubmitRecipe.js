3a075ccfe3c374b62f6196d8f29909db51e800e7
ParthibanChandran <keerthikparthiban@gmail.com>
2020-05-22 10:58:38 +0530

    Doc Comment added

--- src/Pages/SubmitRecipe/SubmitRecipe.js
+++ src/Pages/SubmitRecipe/SubmitRecipe.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import PageTitle from "../../components/PageTitle/PageTitle";
+import PageTitle from "../../commonComponents/PageTitle/PageTitle";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../../styles/theme";
 import { FormContainer, NewIng } from "./style";
